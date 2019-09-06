8d8edc8c5d44fd1e1ffcc330fd40f8b3004757ec
Ochuko Kevwe <kevweochu@gmail.com>
2019-09-06 20:05:15 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
 
 <style lang="scss">
 #app {
+  background-image: url("../src/assets/VoiceOver.jpg");
+   height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home centered">
+    <input type="text" v-model="searchVoice" value="" />
+    <button type="button" @click="getVoice123Actors(searchVoice)" > Search </button>
+    
+  <ul v-for="user in Users" :key="user.id">
+    <li><a href="https://voice123.com/" >{{user.user.username}}</a></li>
+    <li> <img src="../assets/download.png" alt="Avatar" /></li>
+   
+  </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      searchVoice: "",
+      VoiceActors: [],
+      Users: []
+    }
+  },
+  methods: {
+    getVoice123Actors: async function(searchVoice){
+      const res = await fetch(" https://api.sandbox.voice123.com/providers/search/?service=voice_over&keywords=search%20text&page=",{
+        method: "GET"
+      }).then(result => result.json());
+      this.VoiceActors = res.providers;
+      console.log(this.VoiceActors);
+    this.Users = this.VoiceActors.filter(actor => actor.user.username.toLowerCase().startsWith(searchVoice.toLowerCase()));
+     
+    }
+  },
   components: {
-    HelloWorld
   }
 };
 </script>
+
+<style lang="scss" scoped>
+
+.centered{
+    position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px;
+}
+</style>
+
