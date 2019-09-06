5fef69830d233fb981a19892a3944098c6bfad1f
Ochuko Kevwe <kevweochu@gmail.com>
2019-09-06 21:31:29 +0100

    Second Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="home centered">
-    <input type="text" v-model="searchVoice" value="" />
+    <div class="input -group input-div">
+      <h4 style="color:white">Search for a name </h4>
+    <input class="text" type="text" v-model="searchVoice" value="" />
+    </div>
     <button type="button" @click="getVoice123Actors(searchVoice)" > Search </button>
     
   <ul v-for="user in Users" :key="user.id">
-    <li><a href="https://voice123.com/" >{{user.user.username}}</a></li>
+    <li><a style="color:white" v-bind:href="`https://voice123.com/${user.user.username}`"><h2>{{user.user.username}}</h2></a></li>
     <li> <img src="../assets/download.png" alt="Avatar" /></li>
-   
+    <li> <audio controls>
+          <source v-bind:src="`{user.relevant_sample.file}`" type="audio/mpeg">
+            Your browser
+            </audio>
+     </li>
   </ul>
+  
+  <div>
+    <button @click="prevPage">
+    Previous
+  </button>
+  &nbsp;
+   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+  <button @click="nextPage">
+    Next
+  </button>
+  </div>
   </div>
 </template>
 
@@ -20,9 +38,21 @@ export default {
     return {
       searchVoice: "",
       VoiceActors: [],
-      Users: []
+      Users: [],
+      pageNumber: 0
     }
   },
+  props:{
+    listData:{
+      type:Array,
+      required:true
+    },
+    size:{
+      type:Number,
+      required:false,
+      default: 10
+    }
+},
   methods: {
     getVoice123Actors: async function(searchVoice){
       const res = await fetch(" https://api.sandbox.voice123.com/providers/search/?service=voice_over&keywords=search%20text&page=",{
@@ -32,7 +62,20 @@ export default {
       console.log(this.VoiceActors);
     this.Users = this.VoiceActors.filter(actor => actor.user.username.toLowerCase().startsWith(searchVoice.toLowerCase()));
      
-    }
+    },
+    nextPage(){
+         this.pageNumber++;
+      },
+      prevPage(){
+        this.pageNumber--;
+      }
+  },
+  computed: {
+    paginatedData(){
+    const start = this.pageNumber * this.size,
+          end = start + this.size;
+     return this.listData.slice(start, end);
+}
   },
   components: {
   }
@@ -43,10 +86,22 @@ export default {
 
 .centered{
     position: fixed;
-  top: 50%;
-  left: 50%;
-  margin-top: -50px;
-  margin-left: -100px;
+  top: 30%;
+  left: 25%;
+}
+.input-div{
+  display: inline;
+
+}
+.text{
+  width: 600px;
+  height: 50px;
+  margin-bottom: 30px;
+}
+button{
+  width: 150px;
+  height: 50px;
+  margin-bottom: 30px;
 }
 </style>
 
